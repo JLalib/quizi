ad629e33b6e008932959e674f88978e2d227e8fc
JLalib <57844755+JLalib@users.noreply.github.com>
2023-02-06 11:44:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -78,35 +78,66 @@ function clearStatusClass(element) {
 
 const questions = [
   {
-    question: '¿pregunta 1?',
+    question: '¿Cuál fue la causa de la muerte del Profeta Mohamed (SWS)?',
     answers: [
-      { text: 'Ni idea', correct: true },
-      { text: 'Yaqoub', correct: false }
+      { text: 'Envenenado', correct: true },
+      { text: 'Suicidio', correct: false },
+      { text: 'Muerte natural', correct: false }
     ]
-  },
+  }
   {
-    question: 'pregunta 2?',
+    question: '¿Quién será la esposa del Profeta Mohamed (SWS)?',
     answers: [
-      { text: 'respuesta', correct: true },
-      { text: 'respuesta', correct: true },
-      { text: 'respuesta', correct: true },
-      { text: 'respuesta', correct: true }
+      { text: 'Khadija', correct: false },
+      { text: 'Áicha', correct: true },
+      { text: 'Somaia', correct: false }
     ]
-  },
+  }
   {
-    question: 'pregunta 3?',
+    question: 'Nombra un milagro del Profeta Isa (AS)',
     answers: [
-      { text: 'respuesta', correct: false },
-      { text: 'respuesta!!!', correct: true },
-      { text: 'respuesta no', correct: false },
-      { text: 'respuesta', correct: false }
+      { text: 'Resucitar fallecidos', correct: true },
+      { text: 'Hablar con animales', correct: false },
+      { text: 'Predicir el futuro', correct: false }
     ]
-  },
+  }
   {
-    question: 'pregunta 4?',
+    question: '¿Qué profeta le comió un pez?',
     answers: [
-      { text: 'respuesta', correct: false },
-      { text: 'respuesta', correct: true }
+      { text: 'Younes (AS)', correct: false },
+      { text: 'Zakaria (AS)', correct: true },
+      { text: 'Yaqoub (AS)', correct: true }
+    ]
+  }
+  {
+    question: 'Nombra un milagro del Profeta Sulaiman (AS)',
+    answers: [
+      { text: 'Resucitar fallecidos', correct: false },
+      { text: 'Hablar con animales', correct: true },
+      { text: 'Predicir el futuro', correct: false }
+    ]
+  }
+  {
+    question: '¿Qué profeta murió en el cuarto (4º) cielo?',
+    answers: [
+      { text: 'Idris (AS)', correct: true },
+      { text: 'Younes (AS)', correct: false },
+    ]
+  }
+    {
+    question: 'Después de los profetas, ¿Quién entrará primero al paraíso?, pista: es un sahabi.',
+    answers: [
+      { text: 'Abu Bakr Siddiq', correct: true },
+      { text: 'Sulaiman', correct: false },
+      { text: 'Abu Badr', correct: false }
+    ]
+  }
+    {
+    question: 'Quién son los dos (2) ángeles que enseñaron magia en la época de Sulaiman (AS)?',
+    answers: [
+      { text: 'Harut y Marut', correct: true },
+      { text: 'Harut y Faruq', correct: false },
+      { text: 'Faruq y Marut', correct: false }
     ]
   }
 ]
